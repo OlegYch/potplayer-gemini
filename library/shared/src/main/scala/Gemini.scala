226be41f696a2d58e38a90e3d54fb4618923a438
OlegYch <oleglbch@gmail.com>
2025-10-11 20:58:47 +0200

    try to find best models with delayed fallback to worser

--- library/shared/src/main/scala/Gemini.scala
+++ library/shared/src/main/scala/Gemini.scala
@@ -8,10 +8,10 @@ import java.nio.charset.Charset
 import java.time.Instant
 import java.util.concurrent.TimeUnit
 import scala.collection.concurrent.TrieMap
-import scala.concurrent.{Future, blocking}
+import scala.concurrent.{Future, Promise, blocking}
 import scala.util.Random
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration.{Duration, DurationInt}
 
 object Gemini extends Gemini
 class Gemini {
@@ -36,7 +36,7 @@ class Gemini {
 
   // #replaced by build
   val MaxContextLines = 50
-  val Models = List(
+  val Models = Vector(
     "gemini-2.5-flash",
     "gemini-2.0-flash",
     "gemini-2.5-flash-lite",
@@ -47,17 +47,46 @@ class Gemini {
   private case class ModelInfo(results: List[TimedResult]) {
     def addResult(result: Result, delay: Duration) = copy(results = (TimedResult(result, delay, Instant.now) :: results).take(100))
 
-    def delays = results.view.filter(_.res.result.isRight).map(_.d)
-    def errors = results.view.collect { case TimedResult(Result(result = Left(e)), d, ts) => e -> ts }
-    override def toString: String = s"Avg delay: ${delays.map(_.toMillis).sum / delays.size
-        .max(1)}, errors in last 10 secs: ${errors.count(_._2.isAfter(Instant.now.minusSeconds(10)))}"
+    def delays         = results.view.filter(_.res.result.isRight).map(_.d)
+    def succeses       = results.view.collect { case TimedResult(Result(result = Right(e)), d, ts) => e -> ts }
+    def recentSucceses = succeses.filter(_._2.isAfter(Instant.now.minusSeconds(10)))
+    def errors         = results.view.collect { case TimedResult(Result(result = Left(e)), d, ts) => e -> ts }
+
+    val recentErrors = errors.filter(_._2.isAfter(Instant.now.minusSeconds(10)))
+    val dead         = results.size > 100 && recentSucceses.isEmpty
+    val averageDelay = delays.map(_.toMillis).sum / delays.size.max(1)
+
+    override def toString =
+      s"Calls: ${results.size}, Success: ${succeses.size}, Avg delay: $averageDelay, errors in last 10 secs: ${recentErrors.size}"
   }
   private val ModelsInfo = TrieMap[(String, Int), ModelInfo]()
   private def timed[T](f: => Future[T]) = {
     val start = System.currentTimeMillis()
     f.map(_ -> Duration(System.currentTimeMillis() - start, TimeUnit.MILLISECONDS))
   }
-  private def firstSuccess[T](f: List[Future[T]]): Future[T] = {
+  case class WithDelay[T](delay: Promise[Unit], result: Future[T]) {
+    def cancel = delay.tryFailure(new Exception("cancelled"))
+  }
+  private def delayed[T](label: String, d: Duration)(f: => Future[T]) = {
+    val delay = Promise[Unit]()
+    Future {
+      blocking {
+        Thread.sleep(d.toMillis)
+        delay.trySuccess(())
+      }
+    }
+    val withDelay = for {
+      _ <- delay.future.recover {
+        case e =>
+          Logger.println(s"Cancelled $label")
+          throw e
+      }
+      _ = Logger.println(s"Running $label after $d")
+      f <- f
+    } yield f
+    WithDelay(delay, withDelay)
+  }
+  private def firstSuccess[T](f: Seq[Future[T]]): Future[T] = {
     if (f.isEmpty) Future.failed(new Exception("couldn't find success"))
     else
       Future.firstCompletedOf(f).recoverWith {
@@ -68,27 +97,35 @@ class Gemini {
   @transient private var untranslated = Vector[String]()
   @transient private var context      = Vector[(String, String)]()
   def translate(text: String, prompt: Option[String], from: Option[String], to: String, apiKeys: Seq[String]): Future[String] = {
-    val toTranslate = untranslated :+ text
-    val attempts = Models.map { model =>
-      val apiKeyIndex = Random.nextInt(apiKeys.size)
-      val apiKey      = apiKeys(apiKeyIndex)
-      timed(translateImpl(toTranslate.mkString("\n"), context, prompt, from, to, apiKey, model)).map {
-        case (result, d) =>
-          val old = ModelsInfo.get((model, apiKeyIndex)).getOrElse(ModelInfo(Nil))
-          ModelsInfo.update((model, apiKeyIndex), old.addResult(result, d))
-          result.result.fold(
-            e => {
-              Logger.println(s"${model}:${apiKeyIndex} error '$e'")
+    val toTranslate   = untranslated :+ text
+    val defaultModels = Models.flatMap(model => (0 until apiKeys.size).map(model -> _))
+    val bestModels =
+      ModelsInfo.filter(i => !i._2.dead && i._2.recentErrors.size < 2 && i._2.succeses.nonEmpty).toVector.sortBy(_._2.averageDelay)
+    Logger.println(bestModels.mkString("\n"))
+    val currentModels = (bestModels.map(_._1) ++ defaultModels).distinct
+//    Logger.println(currentModels.mkString("\n"))
+    val attempts = currentModels.zipWithIndex.map {
+      case ((model, apiKeyIndex), idx) =>
+        val modelKey = (model, apiKeyIndex)
+        val apiKey   = apiKeys(apiKeyIndex)
+        delayed(modelKey.toString, (700 * idx).millis) {
+          timed(translateImpl(toTranslate.mkString("\n"), context, prompt, from, to, apiKey, model)).map {
+            case (result, d) =>
+              val old = ModelsInfo.get(modelKey).getOrElse(ModelInfo(Nil))
+              ModelsInfo.update(modelKey, old.addResult(result, d))
+              result.result.fold(
+                e => {
+                  Logger.println(s"${model}:${apiKeyIndex} error")
+//                  Logger.println(s"${model}:${apiKeyIndex} error '$e'")
 //            Logger.println(s"${model}:${apiKeyIndex} error '$e' for request:\n${request.asJson}\ncaused by:\n${r.body.toString}")
-              sys.error(e)
-            },
-            res => {
-              res
-            }
-          )
-      }
+                  sys.error(e)
+                },
+                res => res
+              )
+          }
+        }
     }
-    firstSuccess(attempts)
+    firstSuccess(attempts.map(_._2))
       .map { r =>
         untranslated = Vector()
         context = (context :+ (toTranslate.mkString("\n"), r)).takeRight(MaxContextLines)
@@ -100,8 +137,10 @@ class Gemini {
           Logger.println(e)
           "."
       }
-      .andThen { _ =>
-        Logger.println(ModelsInfo.toVector.sortBy(_._1).mkString("\n"))
+      .map { r =>
+        attempts.foreach(_.cancel)
+//        Logger.println(ModelsInfo.toVector.sortBy(_._1).mkString("\n"))
+        r
       }
   }
 
