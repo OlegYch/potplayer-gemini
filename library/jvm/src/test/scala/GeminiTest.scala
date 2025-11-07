e92268aaca89c714f7027d0a7eb2c1ba554fcb86
OlegYch <oleglbch@gmail.com>
2025-11-07 19:40:22 +0100

    fix test

--- library/jvm/src/test/scala/GeminiTest.scala
+++ library/jvm/src/test/scala/GeminiTest.scala
@@ -6,7 +6,7 @@ import sttp.client4.Response
 import scala.concurrent.Future
 
 class GeminiTest(using ExecutionEnv) extends Specification {
-  private val keys  = sys.env("GEMINI_KEYS")
+  private val keys = sys.env("GEMINI_KEYS")
 //  private val model = "gemini-2.0-flash"
   private val model = Gemini.Models.head
   "Hello" in {
@@ -21,13 +21,37 @@ class GeminiTest(using ExecutionEnv) extends Specification {
         ok
     })
   }
-  //todo find a more reliable test
-//  "Hello with context" in {
-//    translateImpl("Hello bill", List("I'm bill" -> "joe"), None, None, "Italian", keys, model).map(_ must beLike {
-//      case Gemini.Result(result = Right("Ciao joe" | "Ciao, Joe.")) =>
-//        ok
-//    })
-//  }
+  // todo find a more reliable test
+  "Hello with context" in {
+    for {
+      resp <- translateImpl(
+        "You're very beatiful",
+        List("Hi i'm Annie, what do you think of me" -> "Ciao, sono Annie, cosa pensi di me?"),
+        None,
+        None,
+        "Italian",
+        keys,
+        model
+      )
+      _ = resp must beLike {
+        case Gemini.Result(result = Right("Sei molto bella.")) =>
+          ok
+      }
+      resp <- translateImpl(
+        "You're very beatiful",
+        List("Hi i'm Bob, what do you think of me" -> "Ciao, sono Bob, cosa pensi di me?"),
+        None,
+        None,
+        "Italian",
+        keys,
+        model
+      )
+      _ = resp must beLike {
+        case Gemini.Result(result = Right("Sei molto bello.")) =>
+          ok
+      }
+    } yield ok
+  }
 //  val fakeGemini = new Gemini {
 //    override def translateImpl(
 //        text: String,
