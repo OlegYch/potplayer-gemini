5ddb2e4235a1c3620aa31f3f48913feddf85dcdf
OlegYch <oleglbch@gmail.com>
2025-11-09 17:54:28 +0100

    refactor

--- library/native/src/main/scala/FixStdio.scala
+++ library/native/src/main/scala/FixStdio.scala
@@ -1,8 +1,7 @@
-import scala.scalanative.unsafe.*
-
 import java.io.{OutputStream, PrintStream}
+import scala.scalanative.*
 import scala.scalanative.libc.stdio
-import scala.scalanative.unsafe.{Zone, toCString}
+import scala.scalanative.unsafe.*
 
 object FixStdio {
   @link("kernel32")
@@ -16,29 +15,28 @@ object FixStdio {
         fileName: CString,
         mode: CString,
         oldStream: Ptr[stdio.FILE]
-    ): scala.scalanative.windows.crt.time.errno_t = extern
+    ): windows.crt.time.errno_t = extern
   }
   def fix(using Zone) = {
-    val ATTACH_PARENT_PROCESS: CUnsignedInt = scala.scalanative.unsigned.UInt.MaxValue
-    val CP_UTF8: CUnsignedInt               = scala.scalanative.unsigned.UInt.valueOf(65001)
+    val ATTACH_PARENT_PROCESS: CUnsignedInt = unsigned.UInt.MaxValue
+    val CP_UTF8: CUnsignedInt               = unsigned.UInt.valueOf(65001)
     Kernel32.SetConsoleOutputCP(CP_UTF8)
-    val stdout      = alloc[Ptr[stdio.FILE]]()
-    val openedError = Kernel32.freopen_s(stdout, toCString("CONOUT$"), toCString("w"), stdio.stdout)
-    if (openedError == 0) {
-      System.setOut(
-        new PrintStream(
-          new OutputStream {
-            def write(b: CInt): Unit = stdio.fputc(b, !stdout)
-          },
-          true,
-          "utf-8"
-        ) {
+    def reopen(name: String, stream: Ptr[libc.stdio.FILE]): Option[PrintStream] = {
+      val handle      = alloc[Ptr[libc.stdio.FILE]]()
+      val openedError = Kernel32.freopen_s(handle, toCString(name), toCString("w"), stream)
+      Option(openedError).filter(_ == 0).map { _ =>
+        val out = new OutputStream {
+          def write(b: CInt): Unit = stdio.fputc(b, !handle)
+        }
+        new PrintStream(out, true, "utf-8") {
           override def println(x: String): Unit = Zone.acquire { implicit zone =>
-            stdio.fputs(toCString(x + "\n"), !stdout)
+            stdio.fputs(toCString(x + "\n"), !handle)
           }
           override def println(x: Any): Unit = println(String.valueOf(x))
         }
-      )
+      }
     }
+    reopen("CONOUT$", stdio.stdout).foreach(System.setOut)
+//    reopen("CONERR$", stdio.stderr).foreach(System.setErr)
   }
 }
